2403859c5abe2bb3527d9d068542a80d1a63dfc7
vassab <113971263+vassab@users.noreply.github.com>
2023-06-16 17:51:13 +0300

    Add files via upload

---
+++ ChessRules/Color.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessRules
+{
+    enum Color
+    {
+        none,
+        White,
+        Black
+    }
+    static class ColorMethods
+    {
+        public static Color FlipColor (this Color color)
+        {
+            if (color == Color.Black) return Color.White;
+            if (color == Color.White) return Color.Black;
+            return Color.none;
+        }
+    }
+}
